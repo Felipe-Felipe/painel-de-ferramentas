a24f11c1a33b7a07745e5a4c0d3574e648d232ac
Felipe-Felipe <fegeatte2@gmail.com>
2026-06-08 22:09:38 -0300

    atualizacao4

--- script.js
+++ script.js
@@ -58,3 +58,78 @@ function brlParaUsd() {
 
 carregarCotacao();
 
+const botao = document.getElementById("btnCalcular");
+
+if (botao) {
+    botao.addEventListener("click", calcularIMC);
+}
+
+function calcularIMC() {
+
+    const peso = Number(document.getElementById("peso").value);
+    const altura = Number(document.getElementById("altura").value);
+    const genero = document.getElementById("genero").value;
+    const resultado = document.getElementById("resultadoimc");
+
+    if (peso <= 0 || altura <= 0 || isNaN(peso) || isNaN(altura)) {
+        resultado.innerHTML = "Digite valores válidos!";
+        return;
+    }
+
+    const imc = peso / (altura * altura);
+
+    let classificacao = "";
+
+    if (genero === "homem") {
+
+        if (imc < 18.5) {
+            classificacao = "Abaixo do peso";
+        } else if (imc <= 24.9) {
+            classificacao = "Normal";
+        } else if (imc <= 29.9) {
+            classificacao = "Sobrepeso";
+        } else {
+            classificacao = "Obesidade";
+        }
+
+    } else {
+
+        if (imc < 18.5) {
+            classificacao = "Abaixo do peso";
+        } else if (imc <= 23.9) {
+            classificacao = "Normal";
+        } else if (imc <= 28.9) {
+            classificacao = "Sobrepeso";
+        } else {
+            classificacao = "Obesidade";
+        }
+
+    }
+
+    resultado.innerHTML =
+        `IMC: ${imc.toFixed(2)}<br>Classificação: ${classificacao}`;
+}
+
+const btn_calculartemperatura = document.getElementById("btnConverter");  
+btn_calculartemperatura.addEventListener("click", calcularTemperatura);
+
+function calcularTemperatura() {
+    const unidadeOrigem = document.getElementById("unidadeOrigem").value;
+    const unidadeDestino = document.getElementById("unidadeDestino").value;
+    const valor = parseFloat(document.getElementById("valorTemperatura").value);
+    const resultado = document.getElementById("resultadotemperatura");    
+    if (isNaN(valor)) {
+        resultado.innerText = "Digite um valor numérico!";
+        return;
+    }
+    let valorConvertido;
+    if (unidadeOrigem === unidadeDestino) {
+        valorConvertido = valor;
+    } else if (unidadeOrigem === "celsius" && unidadeDestino === "fahrenheit") {
+        valorConvertido = (valor * 1.8) + 32;
+    } else if (unidadeOrigem === "fahrenheit" && unidadeDestino === "celsius") {
+        valorConvertido = (valor - 32) / 1.8;
+    } 
+    resultado.innerText = `Valor convertido: ${valorConvertido.toFixed(2)} ${unidadeDestino}`;
+}
+
